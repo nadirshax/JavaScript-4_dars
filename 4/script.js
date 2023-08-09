9181733a39d399f6efb6a0de4ab2f325a3f4ab9a
Nadirshax <103382978+nadirshax@users.noreply.github.com>
2023-08-09 14:14:04 +0500

    JS darslari davomi

---
+++ 4/script.js
@@ -0,0 +1,65 @@
+"use strict"
+
+//ES5
+
+
+
+function getSay() {
+    console.log('JavaScript')
+    console.log(document.body.style.backgroundColor = "black");
+}
+
+////////////////////////////
+
+
+//ES6
+
+
+// (function (text) {
+//   console.log('Hello world ' + text);
+
+// }('JavaScript'))
+
+
+
+
+/////////////////////////////
+
+
+
+
+// function message(text="JavaScript"){
+//   console.log(text);
+// }
+// message()
+
+
+
+
+
+////////////////////////////////////////
+
+
+
+
+// let fruits=new Array('Olma', 'Shaftoli', 'o\'rik', 23, 45);
+// console.log(fruits[2]);
+
+// console.log(typeof(fruits));
+
+
+
+// let carc=['bmw', 23, 'merc', 'audi', 21, 'nissan'];
+
+// let carc=[];
+
+// carc.push('nexia');// oxiridan qo'shadi
+// carc.shift('0');// o'chiradi
+// carc.pop(); //
+// carc.unshift('matiz');// boshidan qo'shadi
+
+// console.log(carc);
+
+
+
+/////////////////////////////////
